f05d67bfbcd3ea4f9969abeeceb8f027e65c4d13
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-05-25 00:13:13 -0300

    commit initial

---
+++ backend/src/test/java/com/matheuslustosa/user_registration/UserRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuslustosa.user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
