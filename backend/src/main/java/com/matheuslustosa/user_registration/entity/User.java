2a51308b02b050fe83ad60bbb4225e935ec9bd6f
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-05-26 15:08:15 -0300

    created entity user

---
+++ backend/src/main/java/com/matheuslustosa/user_registration/entity/User.java
@@ -0,0 +1,59 @@
+package com.matheuslustosa.user_registration.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {
+    }
+
+    public User(UUID id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
