e792e85ba3ae2877b8515d6f7a4a8819b8068a73
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-02 13:45:08 -0300

    add UserNotFoundException and RoleNotFoundException handling

--- backend/src/main/java/com/matheuslustosa/user_registration/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/matheuslustosa/user_registration/exceptions/GlobalExceptionHandler.java
@@ -8,12 +8,62 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ApiResponseDTO<?>>userNotFoundException(UserNotFoundException ex, HttpServletRequest request){
+        log.info("User not found: {} " , ex.getMessage());
+        ApiResponseDTO<?>response=ApiResponseBuilder.error(
+                ErroCodesApi.USER_NOT_FOUND_ERROR.getMessage(),
+                ErroCodesApi.USER_NOT_FOUND_ERROR.getCode(),
+                HttpStatus.NOT_FOUND.value(),
+                request.getRequestURI()
+
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    public ResponseEntity<ApiResponseDTO<?>>roleNotFoundException(RoleNotFoundException ex, HttpServletRequest request){
+        log.info("Role not found: {} " , ex.getMessage());
+        ApiResponseDTO<?>response=ApiResponseBuilder.error(
+                ErroCodesApi.ROLE_NOT_FOUND_ERROR.getMessage(),
+                ErroCodesApi.ROLE_NOT_FOUND_ERROR.getCode(),
+                HttpStatus.NOT_FOUND.value(),
+                request.getRequestURI()
+
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+    }
+
+
+
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ApiResponseDTO<?>>invalidCredentialsException(InvalidCredentialsException ex, HttpServletRequest request){
+        log.warn("Invalid credentials received: {}", ex.getMessage());
+        ApiResponseDTO<?> response = ApiResponseBuilder.error(
+                ErroCodesApi.INVALID_CREDENTIALS.getMessage(),
+                ErroCodesApi.INVALID_CREDENTIALS.getCode(),
+                HttpStatus.UNAUTHORIZED.value(),
+                request.getRequestURI()
+        );
+
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponseDTO<?>> handleGenericException(Exception ex , HttpServletRequest request){
@@ -28,20 +78,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
-
-    @ExceptionHandler(InvalidCredentials.class)
-    public ResponseEntity<ApiResponseDTO<?>>invalidCredentials(InvalidCredentials ex, HttpServletRequest request){
-        log.warn("Invalid credentials received: {}", ex.getMessage());
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponseDTO<?>>AuthorizationDeniedException(AccessDeniedException ex, HttpServletRequest request){
+        log.warn("Authorization denied: {}", ex.getMessage());
         ApiResponseDTO<?> response = ApiResponseBuilder.error(
-                ErroCodesApi.INVALID_CREDENTIALS.getMessage(),
-                ErroCodesApi.INVALID_CREDENTIALS.getCode(),
-                HttpStatus.UNAUTHORIZED.value(),
+                ErroCodesApi.ACCESS_DENIED.getMessage(),
+                ErroCodesApi.ACCESS_DENIED.getCode(),
+                HttpStatus.FORBIDDEN.value(),
                 request.getRequestURI()
+
+
         );
 
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
+
+
 
 }
