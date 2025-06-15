c92ccf40b9a6a2782d8f4eb92007585839bc3f39
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-15 16:29:25 -0300

    rename folder vite-project to frontend

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+function Login(){
+
+    return (
+        <div>
+            <div className="login">
+                
+            </div>
+        </div>
+    );
+    
+
+}
+
+export default Login
