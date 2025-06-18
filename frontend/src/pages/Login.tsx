05dc9b82a9ca53314fc60b5907256a788d6ff4bf
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-18 10:56:04 -0300

    add component Wallpaper

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,10 @@
 import wallpaper from '../assets/images/TaskGt-walpapper.png';
+import { Wallpaper } from '../components/Wallpaper';
 
 function Login() {
   return (
     <div className="flex-1  flex shadow-2xl bg-gradient-to-r from-white via-white to-red-600">
-        <div className='flex flex-1  flex-col justify-center items-center  gap-2'>
-            <h1 className='text-3xl font-serif'>Welcome to <span className='text-red-600'>TaskGT</span></h1>
-            <p className='font-mono'>Your personal tasks here</p>
-            <img src={wallpaper} width={320} alt="" />
-
-        </div>
+        <Wallpaper image={wallpaper} />
         <div className='flex flex-col flex-1 justify-center items-center gap-6 bg-grat '>
             {/* <h2 className='text-2xl'>Enter with your credentials</h2> */}
             <form action="" method="post" className='flex w-md h-8/12 flex-col gap-6 items-center justify-center shadow-2xl rounded-2xl bg-white'>
