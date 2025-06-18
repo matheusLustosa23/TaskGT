764566af017e1bdf516b8fcf6833c05b75a7118f
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-18 11:02:09 -0300

    add border and shadow to footer and nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
+import { Footer } from "./components/Footer";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <nav className="h-28"></nav>
+      <nav className="h-28 border-b-1 border-gray-200 ">
+        <Nav />
+      </nav>
 
-      <main className="flex-1 flex">
+      <main className="flex-1 flex ">
 
+       
       </main>
 
-      <footer className="h-32"></footer>
+      <footer className="h-32 shadow-2xl shadow-black">
+        <Footer />
+      </footer>
     </div>
   );
 }
