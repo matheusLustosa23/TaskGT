f2793063eb1ddad034376fcf9d778805e2a49798
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-18 11:03:23 -0300

    add component Input

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,10 @@
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+
+    
+    return ( 
+    
+    <input className="border-1 border-slate-300 outline-red-600 py-2 px-4 rounded-md" type="text" {...props} />
+
+   );
+
+}
