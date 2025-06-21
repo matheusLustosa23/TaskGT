935830df31e3fafafbc1b5d22a63315d15c5ec1d
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-20 23:33:18 -0300

    alter atribute User

--- frontend/src/types/AuthContextType.ts
+++ frontend/src/types/AuthContextType.ts
@@ -1,9 +1,11 @@
+// import type { ApiResponseType } from "./ApiReponseType"
 import type { loguinRequestType } from "./LoginRequestType"
+// import type { LoginResponseType } from "./LoginResponseType"
 import type { UserType } from "./UserType"
 
 export type AuhtContextType ={
     authenticated:boolean,
-    login:(data:loguinRequestType)=>void,
+    login:(data:loguinRequestType)=>Promise<void>,
     logout:()=>void,
-    user:UserType
+    user:UserType | null
 }
