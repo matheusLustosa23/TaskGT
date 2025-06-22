42ded42e89d450f991ee65299c5154354b7d4a74
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 00:31:55 -0300

    feat: add onClick and options props to Button component

--- frontend/src/components/Buttom.tsx
+++ frontend/src/components/Buttom.tsx
@@ -1,14 +1,18 @@
+
 import type React from "react";
 import { Link } from "react-router-dom";
 
-export function Buttom({width,children,className='',onClick,to}:{width?:string,children:React.ReactNode,className?:string,onClick?:()=>void,to?:string}) {
+export function Buttom(
+    {width,children,className='',onClick,to,...rest}:
+    {width?:string,children:React.ReactNode,className?:string,onClick?:()=>void,to?:string}
+     & React.ButtonHTMLAttributes<HTMLButtonElement>) {
 
     const classBase = `bg-red-600 p-2  rounded-2xl ${width?width:'w-8/12 '} text-white whitespace-nowrap ${className}`;
 
     if(to){
         return(
             <Link to={to}>
-                <button className={classBase} >
+                <button className={classBase} {...rest}>
                       {children}
                 </button>
               
@@ -16,11 +20,20 @@ export function Buttom({width,children,className='',onClick,to}:{width?:string,c
         );
     }
 
-    return (
+    if(onClick){
+         return (
         <button className={classBase} 
-        onClick={() => onClick}>
+        onClick={() => onClick} {...rest}>
             {children}
         
+        </button>
+    );
+    }
+
+    return (
+        <button className={classBase}  {...rest}>
+            {children}
+           
         </button>
     );
 }
