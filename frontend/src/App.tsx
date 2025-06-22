6d3527785dc8b2443b30f3a0238c6f5211fe0c67
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 00:27:42 -0300

    add AuthProvider in the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import {BrowserRouter} from "react-router-dom"
 import { AppRoutes } from "./routes/AppRoutes";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
 
   return(
     <BrowserRouter>
+    <AuthProvider>
       <AppRoutes />
+    </AuthProvider>
     </BrowserRouter>
 
   )
