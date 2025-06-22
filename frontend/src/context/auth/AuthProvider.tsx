41e866ff0027f7ad7709851844e61db08c065aa6
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 19:14:51 -0300

    add loadProfile function and profile state in AuthProvider

--- frontend/src/context/auth/AuthProvider.tsx
+++ frontend/src/context/auth/AuthProvider.tsx
@@ -1,20 +1,37 @@
-import type { AuhtContextType } from "../../types/AuthContextType"
+
 import { AuthContext } from "./AuthContext"
 import { AuthService } from "../../services/AuthService"
 import type { loguinRequestType } from "../../types/LoginRequestType";
 import { useEffect, useState } from "react";
 import { handleApiError } from "../../errors/handleApiError";
+import type { ProfileType } from "../../types/ProfileType";
+import { UserService } from "../../services/UserService";
 
 
 export function AuthProvider({children}:{children:React.ReactNode}){
 
     const [auth,setAuth]=useState(false)
+    const [profile,setProfile]=useState<ProfileType | null>(null)
+
+    const loadProfile = async() =>{
+        try{
+            const profileResponse = await UserService.getProfile()
+            const profile = profileResponse.data || null
+            setProfile(profile)
+        }catch(error:unknown){
+            const err = handleApiError(error,'error loading profile')
+            HandleLogout()
+            alert(err)
+        }
+    }
 
     useEffect(()=>{
         const token=localStorage.getItem('token')
         if(token){
             setAuth(true)
+            loadProfile()
         }
+
     },[]);
     
     
@@ -28,6 +45,8 @@ export function AuthProvider({children}:{children:React.ReactNode}){
             if(token){
                 setAuth(true)
                 localStorage.setItem('token',token)
+                await loadProfile()
+
             }
             
             console.log(`dados recebidos ${response}`)
@@ -42,23 +61,14 @@ export function AuthProvider({children}:{children:React.ReactNode}){
     function HandleLogout(){
         localStorage.removeItem('token')
         setAuth(false)
+        setProfile(null)
 
     }
 
 
-
-
-
-  
-    const authExample:AuhtContextType = {
-        authenticated:auth,
-        login:HandleLogin,
-        logout:HandleLogout,
-        user:{id:'',username:'',roles:['']} //obs
-    }
 
     return(
-        <AuthContext.Provider value={authExample}>
+        <AuthContext.Provider value={{authenticated:auth,login:HandleLogin,logout:HandleLogout,user:profile}}>
             {children}
         </AuthContext.Provider>
     )
