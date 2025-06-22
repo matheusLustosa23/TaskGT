42f05f5597bb54239799d40c9137585be7af4344
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 19:19:16 -0300

    add user context in Home and personalize Wallpaper with username

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,16 @@
 
 import wallpaper from '../assets/images/TaskGt-walpapper3.png';
 import { Wallpaper } from "../components/Wallpaper";
+import { UseAuthContext } from '../context/auth/UseAuthContext';
 
 
 export function Home(){
 
+    const user = UseAuthContext().user
+
     return(
         <div className="flex-1  flex  flex-col items-center gap-2 bg-gradient-to-r from-white via-red-400 to-red-600 justify-center">
-              <Wallpaper title='No tasks found.' text='Got something in mind? Create your first task!' image={wallpaper} />
+              <Wallpaper title='Welcome' highlightedText={` ${user?.username}`}  text='Got something in mind? Create your first task!' image={wallpaper} />
            
         </div>
      
