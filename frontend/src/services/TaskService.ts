266248ae4150830546f33b4473c0d8410fc90654
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 11:59:49 -0300

    add TaskService with getById and getAll methods

---
+++ frontend/src/services/TaskService.ts
@@ -0,0 +1,45 @@
+import type { ApiResponseType } from "../types/ApiReponseType"
+import type { PaginationResponseType } from "../types/PaginationResponseType"
+import type { PaginationRequestType } from "../types/PaginationResquestType"
+import type { TaskType } from "../types/TaskType"
+import { api } from "./api"
+
+
+export const TaskService = {
+    
+    getById:async(id:number):Promise<ApiResponseType<TaskType>> => {
+        const token = localStorage.getItem('token')
+        if(!token){
+            throw('token not found')
+        }
+        const response = await api.get<ApiResponseType<TaskType>>(`/task/${id}`,{
+
+            headers:{Authorization:`Bearer ${token}`}
+        })
+
+        return response.data;
+
+    },
+
+
+    getAll: async(pagination:PaginationRequestType):Promise<ApiResponseType<PaginationResponseType<TaskType[]>>> => {
+        const token = localStorage.getItem('token')
+        if(!token){
+            throw new Error('token not found')
+        }
+        const response = await api.get<ApiResponseType<PaginationResponseType<TaskType[]>>>('/task',{
+            headers:{
+                Authorization:`Bearer ${token}` 
+            },
+            params:{
+                page:pagination.page,
+                pageSize:pagination.pageSize
+            }
+           
+            
+       })
+        return response.data
+    }
+    // updateTask:string,
+    // deleteById:string
+}
