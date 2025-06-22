8227acd48e9e7fce14fa32716d78915a6d30c72a
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 19:11:44 -0300

    create ProfileType

--- frontend/src/types/AuthContextType.ts
+++ frontend/src/types/AuthContextType.ts
@@ -1,11 +1,11 @@
-// import type { ApiResponseType } from "./ApiReponseType"
+
 import type { loguinRequestType } from "./LoginRequestType"
-// import type { LoginResponseType } from "./LoginResponseType"
-import type { UserType } from "./UserType"
+import type { ProfileType } from "./ProfileType"
+
 
 export type AuhtContextType ={
     authenticated:boolean,
     login:(data:loguinRequestType)=>Promise<void>,
     logout:()=>void,
-    user:UserType | null
+    user:ProfileType | null
 }
---
+++ frontend/src/types/ProfileType.ts
@@ -0,0 +1,7 @@
+export type ProfileType = {
+    id:string,
+    username:string,
+    email:string,
+    roles:string[]
+
+}
