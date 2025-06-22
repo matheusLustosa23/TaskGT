aa6ab9dc9c900c81a520291b7db5ed80b54f838e
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-22 19:16:11 -0300

    add highlightedText prop in Wallpaper component

--- frontend/src/components/Wallpaper.tsx
+++ frontend/src/components/Wallpaper.tsx
@@ -1,11 +1,11 @@
-export function Wallpaper(props: {image:string,title?:string,text?:string,width?:number}){
+export function Wallpaper({image,title,text,width,highlightedText}:{image:string,title?:string,text?:string,width?:number,highlightedText?:string}){
     return(
             <div className='flex flex-1  flex-col justify-center items-center  gap-2'>
         
-            {props.title ? <h1 className='text-3xl font-serif'> {props.title }</h1>:<h1 className='text-3xl font-serif'>Welcome to <span className='text-red-600'>TaskGT</span></h1>}
+            {title ? <h1 className='text-3xl font-serif'> {title}{highlightedText?<span className='text-white font-bold underline'>{highlightedText}</span>:''}</h1>:<h1 className='text-3xl font-serif'>Welcome to <span className='text-red-600'>TaskGT</span></h1>}
            
-            <p className='font-mono'>{props.text?props.text:'Your personal tasks here'}</p>
-            <img src={props.image} width={props.width?props.width :320} alt="" />
+            <p className='font-mono'>{text?text:'Your personal tasks here'}</p>
+            <img src={image} width={width?width :320} alt="" />
 
         </div>
     )
