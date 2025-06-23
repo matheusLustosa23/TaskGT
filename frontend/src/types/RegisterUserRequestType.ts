971f6dc272abffdc084f0a0fdeddb032feee3384
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-23 01:11:32 -0300

    add RegisterResponse and request types

---
+++ frontend/src/types/RegisterUserRequestType.ts
@@ -0,0 +1,5 @@
+export type RegisterUserRequestType = {
+    username:string,
+    email:string,
+    password:string
+}
---
+++ frontend/src/types/RegisterUserResponseType.ts
@@ -0,0 +1,5 @@
+export type RegisterUserResponseType = {
+    username:string,
+    email:string
+ 
+}
