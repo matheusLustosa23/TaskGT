c302fd225360c0ba833ab21dee18792438097dcc
Matheus_lustosa <matheus.lustosa23@gmail.com>
2025-06-24 20:33:19 -0300

    add text-nowrap class in ButtomNav

--- frontend/src/components/ButtomNav.tsx
+++ frontend/src/components/ButtomNav.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function ButtomNav({icon,to,children,onClick}:{icon?:React.ReactNode, to?:string,children?:React.ReactNode,onClick?:()=>void})
 {
 
-    const baseClasses = "p-3 bg-red-600 rounded-2xl h-1/2 flex-1 text-white flex flex-row  items-center justify-center hover:bg-red-700 "
+    const baseClasses = "p-3 bg-red-600 rounded-2xl text-nowrap h-1/2 flex-1 text-white flex flex-row  items-center justify-center hover:bg-red-700 "
     
         if(onClick){
             return(
